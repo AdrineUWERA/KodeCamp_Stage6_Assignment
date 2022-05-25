cea41806d5f10bd89d83d7c970134229c3cf1487
Adrine UWERA <a.uwera@alustudent.com>
2022-05-25 20:37:45 +0200

    updated the views file

--- my_portfolio_website/views.py
+++ my_portfolio_website/views.py
@@ -6,14 +6,14 @@ from django.http import HttpResponse
 # function for the home page
 # renders the home html file
 def home(request):
-    return render(request, "home.html", {})
+    return render(request, "home.html")
 
 # function for the about page
 # renders the about html file
 def about(request):
-    return render(request, "about.html", {})   
+    return render(request, "about.html")   
 
 # function for the projects page
 # renders the projects html file
 def projects(request):
-    return render(request, "projects.html", {})   
+    return render(request, "projects.html")   
